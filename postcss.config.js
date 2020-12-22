98f8b6ed153e45eac1e10d749c3c26f7d801fedd
Levi Zitting <me@levizitting.com>
2020-12-21 22:44:23 -0600

    Add cssnano to minify css

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
+const cssnano = require('cssnano');
+
 module.exports = {
     plugins: [
         require('postcss-import'),
         require('tailwindcss')('./tailwind.config.js'),
         require('autoprefixer'),
+        ...(process.env.NODE_ENV !== 'production' ? [] : [
+            cssnano({ preset: 'default' }),
+        ]),
     ],
 };
