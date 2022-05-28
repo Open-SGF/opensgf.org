253481e6f6d51fd445aad1305ec1e8ffeed99fd1
Ethan Zitting <ethan@ethanzitting.com>
2022-05-28 10:38:45 -0500

    initialize and stub out pages

---
+++ pages/about/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../../styles/Home.module.css'
+
+const About: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      About Page
+    </div>
+  )
+}
+
+export default About
---
+++ pages/partner/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../../styles/Home.module.css'
+
+const Partner: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      Partner Page
+    </div>
+  )
+}
+
+export default Partner
