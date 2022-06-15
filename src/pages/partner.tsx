d7a72f496114a9c32dcacb12302ab2fb9ae3ca89
Levi Zitting <me@levizitting.com>
2022-06-14 20:15:36 -0500

    Move pages and components to src

--- src/pages/partner.tsx
+++ src/pages/partner.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import styles from '../styles/Home.module.css';
+import styles from '@/styles/Home.module.css';
 
 const Partner: NextPage = () => {
     return <div className={styles.container}>Partner Page</div>;
--- src/pages/projects/[index].tsx
+++ src/pages/projects/[index].tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import styles from '../../styles/Home.module.css';
+import styles from '@/styles/Home.module.css';
 
 const Project: NextPage = () => {
     return <div className={styles.container}>Project Page</div>;
