439b2abeb078c2bef9b93c94940a6fe7f936f279
Ethan Zitting <ethan@ethanzitting.com>
2022-07-19 20:38:36 -0500

    Delete project-details.tsx

--- src/pages/project-details.tsx
+++
@@ -1,9 +0,0 @@
-import { Gallery } from '@/components/Gallery';
-import { Hero } from '@/components/Blocks/Hero/Hero';
-import Image from 'next/image';
-import { ParticlesComp } from '@/components/Particles/Particles';
-import React from 'react';
-
-export default function ProjectDetails() {
-    return <div></div>;
-}
