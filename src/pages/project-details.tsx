290e20786ade1c870a96117b2d136144a18c5485
zack <zackzbradshaw@gmail.com>
2022-07-19 19:51:51 -0500

    fixed linting issues

--- src/pages/project-details.tsx
+++ src/pages/project-details.tsx
@@ -1,33 +1,11 @@
-import React, {Component} from 'react';
+import { Gallery } from '@/components/Gallery';
 import { Hero } from '@/components/Blocks/Hero/Hero';
-import {ParticlesComp} from "@/components/Particles/Particles";
-import Image from "next/image";
-import Gallery from "@/components/Gallery";
+import Image from 'next/image';
+import { ParticlesComp } from '@/components/Particles/Particles';
+import React from 'react';
 
 export default function ProjectDetails() {
-        return (
-            <div>
-            <Hero
-                header={<h2>Project Name</h2>}
-                text={<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum est harum illum iusto labore reprehenderit repudiandae tenetur velit voluptates!</p>}
-                image={<Image src={"/images/Laptop-phone.svg"} alt='laptop' width={500} height={500}/> }
-                particles={<ParticlesComp />}
-                lable={<h3>Skills/Help Needed</h3>}
-                list={<ul>
-                   <li>
-                       Design
-                   </li>
-                    <li>
-                       Development
-                    </li>
-                    <li>
-                       Marketing
-                    </li>
-                </ul>}
-                bool={true}
-                />
-            <Gallery/>
-            </div>
-        );
+    return (
+        <div></div>
+    );
 }
-
