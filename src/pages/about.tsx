fa74131ac8da220f216ecb8cddf31632180fbf79
Ethan Zitting <ethan@ethanzitting.com>
2022-07-20 08:43:59 -0500

    feat: enable gallery block on /about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,6 @@
 import { IconSet } from '@/components/Blocks/IconSet/IconSet';
 import styles from '@/styles/About.module.scss';
+import {Gallery} from "@/components/Gallery";
 
 export default function About() {
     return (
@@ -10,7 +11,7 @@ export default function About() {
                 A group who takes on tech volunteer work around the community. We meet every Tuesday and host occasional
                 socials. Connect with us and{' '}
             </p>
-            <div>Gallery Block</div>
+            <Gallery/>
         </div>
     );
 }
