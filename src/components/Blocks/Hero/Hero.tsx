0e3b36d302151c932cbb188f234fccb5de4f6889
Ethan Zitting <ethan@ethanzitting.com>
2022-08-12 08:44:17 -0500

    wip: work to fix onscroll events for navbar, refactor various code

--- src/components/Blocks/Hero/Hero.tsx
+++ src/components/Blocks/Hero/Hero.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
 import styles from './Hero.module.scss';
 
-export function Hero(props) {
+export function Hero(props: any) {
     return (
-        <div id="container" className={props.bool ? styles.reversecontainer : styles.container}>
+        <div id="container" className={props.bool ? styles.reverseContainer : styles.container}>
             <div className={styles.message}>
                 <div className={styles.header}>{props.header}</div>
                 <div className={styles.text}>{props.text}</div>
-                <div className={styles.lable}>{props.lable} </div>
+                <div className={styles.label}>{props.lable} </div>
                 <div className={styles.list}>{props.list}</div>
             </div>
             <div className={styles.graphic}>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { Dropdown } from '@/components/Dropdown/Dropdown';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -6,82 +6,67 @@ import { Logo } from '@/components/Icons/Logo';
 import styles from './Navbar.module.scss';
 
 export function Navbar() {
+    const [show, setShow] = useState(true);
+    const [scrollY, setScrollY] = useState(0);
 
-        this.Navlinks = React.createRef();
-
-    function openNav() {
-        const x = this.NavLinks
-        x.style.height = '100%';
-
+    const handleScroll = () => {
+        const { pageYOffset, scrollY } = window;
+        console.log("yOffset", pageYOffset, "scrollY", scrollY);
+        window.scrollY > scrollY ? setShow(false) : setShow(true)
+        setScrollY(window.scrollY);
     }
 
-    function closeNav() {
-        const x = document.getElementById('Navlinks');
-        x.style.height = '0%';
+    const handleClick = (element) => {
+        // TODO: measure if the clicked element is part of the navbar. If not, close the navbar on mobile.
     }
 
-    Navbar = () => {
-        const [show, setShow] = useState(true);
-        const [lastScrollY, setLastScrollY] = useState(0);
-        console.log("scroll")
-
-        const controlNavbar = () => {
-            if (typeof window !== 'undefined') {
-                if (window.scrollY > lastScrollY) {
-                    setShow(false);
-                } else {
-                    setShow(true);
-                }
-
-                setLastScrollY(window.scrollY);
-            }
+    useEffect(() => {
+        const body = document.querySelector('body')
+        if (!body) {
+            return
+        }
+        const options = { passive: true }
+        body.addEventListener("scroll", handleScroll, options);
+        body.addEventListener('click', handleClick)
+        return () => {
+            body.removeEventListener("scroll", handleScroll, options);
+            body.addEventListener('click', handleClick)
         };
+    }, [scrollY]);
 
-        useEffect(() => {
-            if (typeof window !== 'undefined') {
-                window.addEventListener('scroll', controlNavbar);
-
-                return () => {
-                    window.removeEventListener('scroll', controlNavbar);
-                };
-            }
-        }, [lastScrollY]);
-
-        return (
-            <nav className={styles.navbar} ref={this.Navlinks}>
-                <div className={styles.logo}>
-                    <Logo url="/"/>
-                </div>
-                <div className={styles.navlinks} id={'Navlinks'}>
-                    <Link href="/projects" passHref>
-                        <a onClick={closeNav}>Projects</a>
+    return (
+        <nav className={`${styles.navbar} ${show ? styles.open : styles.closed}`}>
+            <div className={styles.logo}>
+                <Logo url="/"/>
+            </div>
+            <div className={styles.navLinks}>
+                <Link href="/projects" passHref>
+                    <a>Projects</a>
+                </Link>
+                <Dropdown heading="Join Us">
+                    <Link href="/partner">
+                        <a>Partnering</a>
                     </Link>
-                    <Dropdown heading="Join Us">
-                        <Link href="/partner">
-                            <a onClick={closeNav}>Partnering</a>
-                        </Link>
-                        <p> See what we can help you with </p>
-                        <Link href="/volunteer">
-                            <a onClick={closeNav}>Volunteering</a>
-                        </Link>
-                        <p>Learn how you can get involved</p>
-                    </Dropdown>
-                    <Link href="/about" passHref onClick={closeNav}>
-                        <a onClick={closeNav}>About</a>
+                    <p> See what we can help you with </p>
+                    <Link href="/volunteer">
+                        <a>Volunteering</a>
                     </Link>
-                    <Link href="/donate" passHref>
-                        <a>
-                            <button className={styles.btn} onClick={closeNav}>
-                                Donate
-                            </button>
-                        </a>
-                    </Link>
-                </div>
-                <a href={'#!'} className={styles.icon} onClick={openNav}>
-                    <Image src="/images/bars-solid.svg" width={30} height={30} alt="bars"/>
-                </a>
-            </nav>
-
-        );
-    };
+                    <p>Learn how you can get involved</p>
+                </Dropdown>
+                <Link href="/about" passHref >
+                    <a>About</a>
+                </Link>
+                <Link href="/donate" passHref>
+                    <a>
+                        <button className={styles.btn} >
+                            Donate
+                        </button>
+                    </a>
+                </Link>
+            </div>
+            <a href={'#!'} className={styles.icon}>
+                <Image src="/images/bars-solid.svg" width={30} height={30} alt="bars"/>
+            </a>
+        </nav>
+    );
 }
