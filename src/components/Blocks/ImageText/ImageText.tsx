94281e7843b51b560ce7e4fd3f212ca78ebd3537
Ethan Zitting <ethan@ethanzitting.com>
2022-08-23 14:39:06 -0700

    wip: fix bug in mirrored ImageText blocks
    Improve page padding on home page at mobile screen widths.
    
    Improve appearance of ImageeText block at mobile screen widths.

--- src/components/Blocks/ImageText/ImageText.tsx
+++ src/components/Blocks/ImageText/ImageText.tsx
@@ -1,5 +1,5 @@
 import { ParticlesComp } from '@/components/Particles/Particles';
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import styles from './ImageText.module.scss';
 
 interface IImageText {
@@ -10,6 +10,31 @@ interface IImageText {
     imageTextSizeRatio?: number;
 }
 
+const useMediaQuery = (width: number) => {
+    const [targetReached, setTargetReached] = useState(false);
+
+    const updateTarget = useCallback((e: any) => {
+        if (e.matches) {
+            setTargetReached(true);
+        } else {
+            setTargetReached(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        const media = window.matchMedia(`(max-width: ${width}px)`);
+        media.addEventListener('change', updateTarget);
+
+        if (media.matches) {
+            setTargetReached(true);
+        }
+
+        return () => media.removeEventListener('change', updateTarget);
+    }, [updateTarget, width]);
+
+    return targetReached;
+};
+
 export function ImageText({
     image,
     text,
@@ -17,12 +42,18 @@ export function ImageText({
     enableParticles = false,
     textRight = false,
 }: IImageText): JSX.Element {
+    const isBreakpoint = useMediaQuery(600);
+
+    const textWrapperWidth = isBreakpoint ? '100%' : imageTextSizeRatio * 100 + '%';
+
+    const graphicWrapperWidth = isBreakpoint ? '100%' : (1 - imageTextSizeRatio) * 100 + '%';
+
     return (
-        <div className={`${styles.container} ${textRight ? 'textRight' : ''}`}>
-            <div className={styles.textWrapper} style={{ width: imageTextSizeRatio * 100 + '%' }}>
+        <div className={`${styles.container} ${textRight ? styles.textRight : ''}`}>
+            <div className={styles.textWrapper} style={{ width: textWrapperWidth }}>
                 {text}
             </div>
-            <div className={styles.graphicWrapper} style={{ width: (1 - imageTextSizeRatio) * 100 + '%' }}>
+            <div className={styles.graphicWrapper} style={{ width: graphicWrapperWidth }}>
                 <div className={styles.image}>{image}</div>
                 {enableParticles && (
                     <div className={styles.particles}>
