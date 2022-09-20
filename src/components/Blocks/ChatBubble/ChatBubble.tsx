9f18773772cced6a3d6a71367ba5625e5fdce532
Ethan Zitting <ethan@ethanzitting.com>
2022-09-20 11:21:56 -0500

    Refine styling of ChatBubble block
    Add variable light-blue.
    
    Extract ChatBubble SVGs into their own components.
    
    Make ChatBubble SVG color editable.
    
    Refine responsiveness of side margins of individual project page.
    
    Simplify JSX of ChatBubble component.

--- src/components/Blocks/ChatBubble/ChatBubble.tsx
+++ src/components/Blocks/ChatBubble/ChatBubble.tsx
@@ -1,43 +1,31 @@
-import Image from 'next/image';
+import { ChatBubbleLeft } from '@/components/Icons/ChatBubbleLeft/ChatBubbleLeft';
+import { ChatBubbleRight } from '@/components/Icons/ChatBubbleRight/ChatBubbleRight';
 import styles from './ChatBubble.module.scss';
 
 export function ChatBubble(): JSX.Element {
+    const clientIssue =
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis.';
+    const ourSolution =
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis.';
+
     return (
         <div className={styles.container}>
-            <div className={styles.row}>
-                <div className={styles.columnOne}>
-                    <div className={styles.columnBubble}>
-                        <Image
-                            src="/images/light-blue-chat-bubble.svg"
-                            alt="A chat bubble about a client issue"
-                            width="526.03px"
-                            height="300px"
-                        />
-                        <div className={styles.bubbleChat}>
-                            <div className={`${styles.bubbleHeader} h1`}>Client Issue</div>
-                            <div className={styles.chat}>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis.
-                            </div>
-                        </div>
-                    </div>
+            <div className={styles.bubble}>
+                <div className={styles.imageWrapper}>
+                    <ChatBubbleLeft />
                 </div>
-                <div className={styles.columnTwo}>
-                    <div className={styles.columnBubble}>
-                        <Image
-                            src="/images/dark-blue-chat-bubble.svg"
-                            alt="A chat bubble about a client issue"
-                            width="526.03px"
-                            height="300px"
-                        />
-                        <div className={styles.bubbleChat}>
-                            <div className={`${styles.bubbleHeader} h1`}>Our Solution</div>
-                            <div className={styles.chat}>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis.
-                            </div>
-                        </div>
-                    </div>
+                <div className={styles.textWrapper}>
+                    <div className={`${styles.title} h1`}>Client Issue</div>
+                    <div className={styles.body}>{clientIssue}</div>
+                </div>
+            </div>
+            <div className={styles.bubble}>
+                <div className={styles.imageWrapper}>
+                    <ChatBubbleRight />
+                </div>
+                <div className={styles.textWrapper}>
+                    <div className={`${styles.title} h1`}>Our Solution</div>
+                    <div className={styles.body}>{ourSolution}</div>
                 </div>
             </div>
         </div>
