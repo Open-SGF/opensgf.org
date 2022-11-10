d00b4a5ab72e5648146d2395c90315f897ee70f6
Ethan Zitting <ethan@ethanzitting.com>
2022-11-09 21:14:20 -0600

    Refine styling of gallery block (#125)
    Move project images into own directory

--- src/components/Blocks/Gallery/Gallery.tsx
+++ src/components/Blocks/Gallery/Gallery.tsx
@@ -8,9 +8,10 @@ interface IGallery {
 export function Gallery({ images }: IGallery): JSX.Element {
     return (
         <div className={styles.container}>
-            {images.map((image) => (
-                <Image src={image.src} alt={image.alt} key={image.src} fill />
-            ))}
+            <Image src={images[0].src} alt={images[0].alt} key={images[0].src} fill />
+            <Image src={images[1].src} alt={images[1].alt} key={images[1].src} fill />
+            <Image src={images[2].src} alt={images[2].alt} key={images[2].src} fill />
+            <Image src={images[3].src} alt={images[3].alt} key={images[3].src} fill />
         </div>
     );
 }
