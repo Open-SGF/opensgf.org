d2fdf9db4953095d6adbc9885eaf126a7c6e07a5
Ethan Zitting <ethan@ethanzitting.com>
2022-11-15 19:16:07 -0600

    Fix build errors

--- src/pages/api/github.ts
+++ src/pages/api/github.ts
@@ -1,6 +1,6 @@
 import { Octokit } from 'octokit';
 
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
     const octokit = new Octokit();
     const response = await octokit.request('GET ' + req.query.apiUrl);
     res.status(200).json(response.data);
