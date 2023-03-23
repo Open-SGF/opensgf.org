8a00a1daf95867ca7abcff733a33892a03495c1a
Ethan Zitting <ethan@ethanzitting.com>
2023-03-23 13:20:56 -0500

    Remove ex-founders from about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,26 +8,10 @@ export default function About(): JSX.Element {
     const teamIcons: Icon[] = [
         {
             image: {
-                src: '/images/people/fred-lawler-headshot.jpg',
-                alt: 'Image of Fred Lawler',
-            },
-            title: <figcaption className={'h3 ' + styles.name}>Fred Lawler</figcaption>,
-            subtitle: <figcaption className={'h6 ' + styles.position}>Co-Organizer</figcaption>,
-        },
-        {
-            image: {
                 src: '/images/people/levi-zitting-headshot.jpg',
                 alt: 'Image of Levi Zitting',
             },
             title: <figcaption className={'h3 ' + styles.name}>Levi Zitting</figcaption>,
-            subtitle: <figcaption className={'h6 ' + styles.position}>Co-Organizer</figcaption>,
-        },
-        {
-            image: {
-                src: '/images/people/ethan-zitting-headshot.jpg',
-                alt: 'Image of Ethan Zitting',
-            },
-            title: <figcaption className={'h3 ' + styles.name}>Ethan Zitting</figcaption>,
             subtitle: <figcaption className={'h6 ' + styles.position}>Co-Organizer</figcaption>,
         },
     ];
