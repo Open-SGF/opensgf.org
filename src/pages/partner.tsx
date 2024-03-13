5444f0b80224f3a9d9cf076c6e08ae7eeb522266
Daniel Moore <dmoore1987@gmail.com>
2024-03-12 20:13:41 -0500

    ' != "
    think about it

--- src/pages/partner.tsx
+++ src/pages/partner.tsx
@@ -1,6 +1,6 @@
 import { ContactForm } from '@/components/Blocks/ContactForm/ContactForm';
 import React from 'react';
-import { Sponsers } from "@/components/Blocks/Sponsers/Sponsers";
+import { Sponsers } from '@/components/Blocks/Sponsers/Sponsers';
 import styles from '@/styles/pages/Partner.module.scss';
 
 export default function partner(): JSX.Element {
