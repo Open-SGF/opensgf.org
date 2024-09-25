9cdab00ea106b9d3e029114eb3443f24f42ca295
Thomas Hammon <hammonthomas@gmail.com>
2024-09-25 15:22:58 -0500

    method conference banner, now linted

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -1,35 +1,32 @@
-import React from "react";
+import React from 'react';
 import styles from './banner.module.scss';
 
-
-export default function Banner(){
-
-    const [displayBanner, setDisplayBanner] = React.useState(true)
+export default function Banner() {
+    const [displayBanner, setDisplayBanner] = React.useState(true);
 
     const display = displayBanner ? styles.banner : styles.hidden;
 
-    function toggleBanner(){
-        setDisplayBanner(prevState => !prevState)
+    function toggleBanner() {
+        setDisplayBanner((prevState) => !prevState);
     }
 
     return (
-            <div className={display}>
-                <div>
-                    <h3 className={styles.textBold}>Method Conference</h3>
-                </div>
-                <div className={styles.textArea}>
-                    <p className={styles.textBold}>Join us for a full day of code & learning.</p>
-                    <p className={styles.text}>Saturday, October 12, 2024, @ eFactory</p>
-                </div>
-                <div className={styles.container}>
-                    <a href="https://www.methodconf.com/2024/" target="_blank">
-                        <button className={styles.button}>BUY TICKETS</button>
-                    </a>
-                    <button className={styles.exitButton} onClick={toggleBanner}>
-                        <img src="/images/icons/exit-cross.png" className={styles.exit} alt="close banner"/>
-                    </button>
-                </div>
+        <div className={display}>
+            <div>
+                <h3 className={styles.textBold}>Method Conference</h3>
             </div>
-        ) 
-        
+            <div className={styles.textArea}>
+                <p className={styles.textBold}>Join us for a full day of code & learning.</p>
+                <p className={styles.text}>Saturday, October 12, 2024, @ eFactory</p>
+            </div>
+            <div className={styles.container}>
+                <a href="https://www.methodconf.com/2024/" target="_blank">
+                    <button className={styles.button}>BUY TICKETS</button>
+                </a>
+                <button className={styles.exitButton} onClick={toggleBanner}>
+                    <img src="/images/icons/exit-cross.png" className={styles.exit} alt="close banner" />
+                </button>
+            </div>
+        </div>
+    );
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Footer } from '@/components/Footer/Footer';
 import { Navbar } from '@/components/Navbar/Navbar';
-import Banner from '@/components/Banner/banner.jsx'
+import Banner from '@/components/Banner/banner.jsx';
 import styles from './layout.module.scss';
 
 interface ILayout {
@@ -9,7 +9,6 @@ interface ILayout {
 }
 
 export function Layout({ children }: ILayout): JSX.Element {
-
     return (
         <div className={styles.pageContainer}>
             <div className={styles.contentWrapper}>
