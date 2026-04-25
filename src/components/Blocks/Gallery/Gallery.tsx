338f72b73fd8ec962b0a6f91ef968ca1785d1cb6
Levi Zitting <me@levizitting.com>
2026-04-24 21:06:53 -0500

    fix: css issues and key warnings

--- src/components/Blocks/Gallery/Gallery.tsx
+++ src/components/Blocks/Gallery/Gallery.tsx
@@ -8,10 +8,14 @@ interface IGallery {
 export function Gallery({ images }: IGallery): React.ReactNode {
     return (
         <div className={styles.container}>
-            <Image src={images[0].src} alt={images[0].alt} key={images[0].src} fill />
-            <Image src={images[1].src} alt={images[1].alt} key={images[1].src} fill />
-            <Image src={images[2].src} alt={images[2].alt} key={images[2].src} fill />
-            <Image src={images[3].src} alt={images[3].alt} key={images[3].src} fill />
+            {images.map((image, index) => (
+                <Image
+                    key={index}
+                    src={image.src}
+                    alt={image.alt}
+                    fill
+                />
+            ))}
         </div>
     );
 }
