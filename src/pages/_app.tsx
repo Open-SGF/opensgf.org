01ae43ec846397227027987aaee2bf5443663c1d
Levi Zitting <me@levizitting.com>
2026-04-24 19:54:08 -0500

    fix: update next-plausible for v4 API changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
                 <link rel="manifest" href="/site.webmanifest" />
                 <title>Open SGF</title>
             </Head>
-            <PlausibleProvider domain="opensgf.org" customDomain="https://plausible.sgf.dev">
+            <PlausibleProvider src="https://plausible.sgf.dev/js/script.js">
                 <Component {...pageProps} />
             </PlausibleProvider>
         </Layout>
